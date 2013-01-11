4321efeeb7f027a98265751781f228a29b77f318
Will Szumski <will@cowboycoders.org>
2013-01-11 01:33:11 +0000

    Add course list start

---
+++ MyTracksLib/gen/com/google/android/apps/mytracks/lib/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.google.android.apps.mytracks.lib;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
