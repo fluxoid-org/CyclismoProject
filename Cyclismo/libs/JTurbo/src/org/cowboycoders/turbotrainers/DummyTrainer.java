c6d87a36911bc622c5f4a34f339908a23a696a41
Will Szumski <will@cowboycoders.org>
2015-03-21 15:06:57 +0000

    add missing files

---
+++ Cyclismo/libs/JTurbo/src/org/cowboycoders/turbotrainers/DummyTrainer.java
@@ -0,0 +1,84 @@
+package org.cowboycoders.turbotrainers;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Created by fluxoid on 17/03/15.
+ */
+public class DummyTrainer extends GenericTurboTrainer {
+
+    public static final Mode [] SUPPORTED_MODES = new Mode [] {
+            Mode.TARGET_SLOPE
+    };
+
+    {
+        setSupportedModes(SUPPORTED_MODES);
+    }
+
+    private double distance = 0.0;
+
+    private final class DataSpoofer extends TimerTask {
+        @Override
+        public void run() {
+            for (TurboTrainerDataListener listener: getDataChangeListeners()) {
+                listener.onCadenceChange(75);
+                listener.onPowerChange(250);
+                listener.onHeartRateChange(132);
+                listener.onSpeedChange(6.7056);
+                distance += 6.7056;
+                listener.onDistanceChange(distance);
+            }
+
+        }
+    };
+
+    private Timer dataTimer =  null;
+
+
+    @Override
+    public boolean supportsSpeed() {
+        return true;
+    }
+
+    @Override
+    public boolean supportsPower() {
+        return true;
+    }
+
+    @Override
+    public boolean supportsCadence() {
+        return true;
+    }
+
+    @Override
+    public boolean supportsHeartRate() {
+        return true;
+    }
+
+    @Override
+    public void setParameters(Parameters.CommonParametersInterface parameters) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public double getTarget() {
+        return 0;
+    }
+
+    @Override
+    public void start() throws TurboCommunicationException, InterruptedException, TimeoutException, IllegalStateException {
+        dataTimer =  new Timer(true);
+        // spoof some data every second
+        dataTimer.scheduleAtFixedRate(new DataSpoofer(), 2000, 1000);
+    }
+
+    @Override
+    public void stop() throws InterruptedException, TimeoutException {
+        if (dataTimer != null) {
+            dataTimer.cancel();
+            dataTimer = null;
+        }
+    }
+}
