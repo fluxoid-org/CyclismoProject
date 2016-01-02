0c0ddaa4c4955b2af601fefbc7b4169ba12fde4a
Doug Szumski <d.s.szumski@gmail.com>
2016-01-02 18:52:20 +0000

    Add method and test for interpolating between points

--- src/org/fluxoid/utils/Conversions.java
+++ src/org/fluxoid/utils/Conversions.java
@@ -6,6 +6,7 @@ public class Conversions {
 	
 	// make sure we aren't truncating to int
 	public static final double METRES_PER_SECOND_TO_KM_PER_HOUR = 60 * 60 / 1000.;
+  public static final double MILLISECONDS_IN_SECOND = 1000.0;
 	
 	public static final double KM_PER_HOUR_TO_METRES_PER_SECOND = (1 / METRES_PER_SECOND_TO_KM_PER_HOUR);
 	
--- test/org/fluxoid/utils/LocationUtilsTest.java
+++ test/org/fluxoid/utils/LocationUtilsTest.java
@@ -1,8 +1,8 @@
 package org.fluxoid.utils;
 
-import org.fluxoid.utils.LatLongAlt;
-import org.fluxoid.utils.LocationUtils;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.util.List;
 
@@ -17,7 +17,12 @@ public class LocationUtilsTest {
     private static final LatLongAlt landsEnd = new LatLongAlt(50.0664, -5.7147, 0.0);
     private static final LatLongAlt johnoGroats = new LatLongAlt(58.6439, -3.07, 0.0);
 
-    @Test
+    // Two points about 50m apart
+    private static final LatLongAlt SRC = new LatLongAlt(51.38026, 12.35125, 123);
+    private static final LatLongAlt DST = new LatLongAlt(51.37993, 12.35073, 122);
+
+
+  @Test
     public void testGradient() {
         // No height difference between points
         assertEquals(0.0, LocationUtils.getLocalisedGradient(landsEnd, johnoGroats), TINY);
@@ -38,21 +43,44 @@ public class LocationUtilsTest {
 
     @Test
     public void testInterpolateBetweenPoints() {
-        // Two points about 50m apart, going down a slight hill
-        LatLongAlt src = new LatLongAlt(51.38026, 12.35125, 123);
-        LatLongAlt dst = new LatLongAlt(51.37993, 12.35073, 122);
         double maxSpacing = 2.0;
 
         // Interpolate between the two points
-        List<LatLongAlt> points = LocationUtils.interpolateBetweenPoints(src, dst, maxSpacing);
+        List<LatLongAlt> points = LocationUtils.interpolateBetweenPoints(SRC, DST, maxSpacing);
 
         // Check that the interpolated points do not exceed the maximum spacing
-        LatLongAlt last = src;
+        LatLongAlt last = SRC;
         for (LatLongAlt point: points) {
             assertTrue(maxSpacing >= LocationUtils.getDistance(last, point));
             last = point;
         }
-
     }
 
+  @Test
+  public void testGetLocationBetweenPointsValid() {
+    LatLongAlt nowhere = LocationUtils.getLocationBetweenPoints(SRC, DST, 50.0, 0.0);
+    assertEquals(SRC.getLatitude(), nowhere.getLatitude(), TINY);
+    assertEquals(SRC.getLongitude(), nowhere.getLongitude(), TINY);
+    assertEquals(SRC.getAltitude(), nowhere.getAltitude(), TINY);
+
+    LatLongAlt halfway = LocationUtils.getLocationBetweenPoints(SRC, DST, 25.0, 1000.0);
+    assertEquals(51.38010, halfway.getLatitude(), TINY);
+    assertEquals(12.35100, halfway.getLongitude(), TINY);
+    assertEquals(122.5, halfway.getAltitude(), COARSE);
+
+    LatLongAlt end = LocationUtils.getLocationBetweenPoints(SRC, DST, 50.0, 1000.0);
+    assertEquals(DST.getLatitude(), end.getLatitude(), TINY);
+    assertEquals(DST.getLongitude(), end.getLongitude(), TINY);
+    assertEquals(DST.getAltitude(), end.getAltitude(), COARSE);
+  }
+
+  @Rule
+  public final ExpectedException exception = ExpectedException.none();
+
+  @Test
+  public void testGetLocationBetweenPointsInvalid() {
+    exception.expect(RuntimeException.class);
+    // ~50m past the destination
+    LocationUtils.getLocationBetweenPoints(SRC, DST, 100.0, 1000.0);
+  }
 }
