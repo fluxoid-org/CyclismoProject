d00fcec085fdd93867df8c6df312c03744f5387f
Will Szumski <will@cowboycoders.org>
2017-08-31 23:30:56 +0100

    make FormicaProfiles depend on fluxutils from tree

--- Cyclismo/libs/FormicaProfiles/libs/cp.sh
+++ Cyclismo/libs/FormicaProfiles/libs/cp.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 JFORMICA_DIR=~/code/CyclismoProject/Cyclismo/libs/jformica
-FLUXUTILS_DIR=~/code/CyclismoProject/Cyclismo/libs/FluxUtils
-JTURBO_DIR=~/code/CyclismoProject/Cyclismo/libs/JTurbo
 
 cp "$JFORMICA_DIR"/jformica_core/build/libs/*.jar .
 cp "$JFORMICA_DIR"/jformica_jsr80/build/libs/*.jar .
-cp "$FLUXUTILS_DIR"/build/libs/*.jar .
-cp "$JTURBO_DIR"/build/libs/*.jar .
+
 rm ./*-sources.jar
 rm ./*-javadoc.jar
