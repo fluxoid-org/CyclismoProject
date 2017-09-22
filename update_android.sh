171e11e2f0eeb31832b645db8508edcd0394a414
Will Szumski <will@cowboycoders.org>
2017-09-22 02:58:44 +0100

    do the travis shuffle

--- update_android.sh
+++ update_android.sh
@@ -1,7 +1,7 @@
 sudo apt-get install -y expect
 expect -c '
 set timeout -1;
-spawn android update sdk --no-ui --all --filter tools,platform-tools,build-tools-25.0.0
+spawn android update sdk --no-ui --all --filter tools,platform-tools,build-tools-26.0.1
 expect {
     "Do you accept the license" { exp_send "y\r" ; exp_continue }
     eof
